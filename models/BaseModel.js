4f042fa5b0e8794f9fe5e992326b44b870d45f67
Ty Luu <dinhty.luu@gmail.com>
2017-10-27 18:22:12 +0700

    Update

--- models/BaseModel.js
+++ models/BaseModel.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
   host     : 'localhost',
-  port     : '8889',
+  port     : '3306',
   user     : 'root',
   password : 'root',
   database : 'espn_crawl',
